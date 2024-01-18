c89c5e9694fe94c00e66135086cca67b9285693b
iwantlatiao <n.baaa@163.com>
2024-01-18 20:29:11 +0800

    Create luogu_P4924.py

---
+++ luogu_P4924.py
@@ -0,0 +1,13 @@
+def rotate_arr(x, y, r, z):
+    startX, startY = x - r, y - r
+    endX, endY = x + r, y + r
+    tmpArr = [[arr[i][j] for j in range(startY, startY+1)]
+              for i in range(startX, endX+1)]
+
+
+n, m = map(int, input().split())
+arr = [[n*i+j for j in range(1, n+1)] for i in range(n)]
+
+
+
+print(arr)
