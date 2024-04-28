c23ec55585c729a058dca2a0c3d44a9c9bd8801e
iwantlatiao <n.baaa@163.com>
2024-04-28 15:08:35 +0800

    Update luogu_P1827.py

--- luogu_training_list/2-2_binaryTree/luogu_P1827.py
+++ luogu_training_list/2-2_binaryTree/luogu_P1827.py
@@ -1,8 +1,24 @@
 import sys
 
-def dfs(localPre, localIn):
-    
+sys.setrecursionlimit(1000000)
+
+
+def dfs(inOrder: str, preOrder: str):
+    root = preOrder[0]
+    rootIndex = inOrder.find(root)  # 中序遍历 根的位置
+
+    # 如果有左子树就遍历左子树
+    if rootIndex > 0:
+        dfs(inOrder[:rootIndex], preOrder[1 : rootIndex + 1])
+
+    # 如果有右子树就遍历右子树
+    if rootIndex < len(inOrder) - 1:
+        dfs(inOrder[rootIndex + 1 :], preOrder[rootIndex + 1 :])
+
+    print(root, end="")
+
 
 input = sys.stdin.readline
-preOrder = input().strip()
 inOrder = input().strip()
+preOrder = input().strip()
+dfs(inOrder, preOrder)
