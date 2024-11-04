1dad16809598e6db68c39e1c9af8b8b9fb5de662
iwantlatiao <77711330+iwantlatiao@users.noreply.github.com>
2024-11-04 23:17:41 +0800

    Update acwing_174.cpp

--- AGAC/0x20/acwing_174.cpp
+++ AGAC/0x20/acwing_174.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <vector>
 #include <queue>
 using namespace std;
@@ -25,6 +25,28 @@ struct Point {
     int x, y, dir;
 } pre[N][N][4];  // 用于求箱子和人的移动路径
 
+bool bfs_man(PII start, PII end, PII box, vector<int> &seq) {
+    return false;
+}
+
+// 对箱子做 bfs，如果能到达终点，end 保存最短到达的终点位置和方向
+bool bfs_box(PII start, PII box, Point &end) {
+    memset(st, false, sizeof st);
+    queue<Point> q;
+    
+    // 起始状态插入队列
+    for (int i = 0; i < 4; i++) {
+        int a = box.first + dx[i], b = box.second + dy[i];  // 人要走到的位置
+        int c = box.first - dx[i], d = box.second - dy[i];  // 箱子推到的位置
+        if (!check(a, b) || !check(c, d)) continue;
+
+        vector<int> seq;
+        int len = bfs_man(start, PII {a, b}, box, seq);
+    }
+    
+    return false;
+}
+
 int main() {
     int T = 0;
     while (cin >> n >> m && n > 0 && m > 0) {
@@ -40,7 +62,10 @@ int main() {
         
         Point end;   // 用 (x, y, dir) 存，便于最后倒着找路径
         string res;  // 记录最终路径答案
-        
+        if (!bfs_box(start, box, end)) res = "Impossible.";
+        else {
+            //
+        }
     }
 
     return 0;
